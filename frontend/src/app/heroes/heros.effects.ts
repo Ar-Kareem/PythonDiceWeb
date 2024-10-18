f64f90ba3b9cf400401714476d43b8d74c226906
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-10-18 09:09:16 -0400

    separate service from effect

--- frontend/src/app/heroes/heros.effects.ts
+++ frontend/src/app/heroes/heros.effects.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, catchError, switchMap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 import { CodeApiActions } from './heros.reducer';
+import { HerosService } from './code.service';
 
 
 // EFFECTS
@@ -51,56 +50,3 @@ export class HerosEffects {
   ) {}
 }
 
-
-// SERVICE
-function myStreamHandler(source: Observable<string>) {
-  return source.pipe(
-    map((response: string) => JSON.parse(response.trim())),
-    map((response: any) => {
-      if (!!response.is_error) {
-        throw response;
-      }
-      return response;
-    })
-  );
-}
-
-@Injectable({ providedIn: 'root' })
-export class HerosService {
-  constructor(private http: HttpClient) {}
-
-  postCode(code: string): Observable<any> {
-    return this.http.post('/api/ParseExec', { code }, {responseType: 'text', }).pipe(
-      myStreamHandler,
-      map((response: any) => {
-        if (!response || !response.result || response.result.trim() == '') {
-          response.payload = 'No output found. Did you forget to call "output"?';
-          response.message = 'CUSTOM';
-          throw response
-        }
-        return response
-      }),
-    );
-  }
-
-  postPythonCode(code: string): Observable<any> {
-    return this.http.post('/api/ExecPython', { code }, {responseType: 'text', }).pipe(
-      myStreamHandler,
-      map((response: any) => {
-        if (!response || !response.result || response.result.trim() == '') {
-          response.payload = 'No output found. Did you forget to call "output"?';
-          response.message = 'CUSTOM';
-          throw response
-        }
-        return response
-      }),
-    );
-  }
-
-  postTranslateCode(code: string): Observable<any> {
-    return this.http.post('/api/Translate', { code }, {responseType: 'text', }).pipe(
-      myStreamHandler,
-      map((response: any) => response),
-    );
-  }
-}
