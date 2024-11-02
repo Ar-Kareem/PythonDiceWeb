6cfefa16a062e139922f7786fe4c64b4a8d5a14c
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-11-03 01:39:25 +0300

    dont get program if key isn't int

--- frontend/src/app/getprog.service.ts
+++ frontend/src/app/getprog.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpContext } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 import { API_BLOCKS_UI } from './http-interceptor';
 
@@ -11,6 +12,9 @@ export class GetprogService {
   constructor(private http: HttpClient) { }
 
   getprog(id: number|string) {
+    if (!isNumber(id)) {
+      return of({"error":"key must be an integer","resp":"error"})
+    }
     const url = `https://pydiceapi.abdulrahman-kareem.com/get_prog`;
     return this.http.post(url, {key: id}, {
       context: new HttpContext().set(API_BLOCKS_UI, true),
@@ -23,4 +27,9 @@ export class GetprogService {
       context: new HttpContext().set(API_BLOCKS_UI, true),
     });
   }
+}
+
+function isNumber(n: number|string) {  // https://stackoverflow.com/a/27285032/6173665
+  // @ts-ignore
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
