c3243e40334cf8b91d16a99918d8d00142419e53
Ar-Kareem <abdulrahman_3000@hotmail.com>
2024-11-08 21:39:26 +0300

    fix accoardion

--- frontend/src/app/heroes/docs/docs.component.ts
+++ frontend/src/app/heroes/docs/docs.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { LocationStrategy } from '@angular/common'; 
 import { herosSelectors, SidebarActions } from '../heros.reducer';
+import Utils from '@app/utils';
 
 @Component({
   selector: 'app-docs',
@@ -12,18 +13,30 @@ export class DocsComponent implements AfterViewInit {
 
   visible = false
   baseHref: string = this.locationStrategy.getBaseHref();
-  activeIndex = [0];
+  activeIndex: number[] = [];
 
   constructor(
     private store: Store,
     private locationStrategy: LocationStrategy,
+    private cd: ChangeDetectorRef,
   ) { }
 
   ngAfterViewInit() {
     this.store.select(herosSelectors.selectDocsVisible).subscribe(visible => {
-      console.log('DOCS VISIBLE', visible);
+      this.accordianBugFix()
       this.visible = visible
     });
+    if (typeof window !== 'undefined') {
+      (window as any).docs = this
+    }
+  }
+
+  accordianBugFix() {
+    // https://github.com/primefaces/primeng/issues/12174
+    this.activeIndex = Utils.range(0, 100)
+    this.cd.detectChanges()
+    this.activeIndex = []
+    this.cd.detectChanges()
   }
 
   onHide() {
